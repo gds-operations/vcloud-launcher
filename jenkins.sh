7b27f60ec2c8d20e1810e8325816728b6afa750b
Anna Shipman <anna@annashipman.co.uk>
2014-04-02 15:40:00 +0100

    Jenkins to run one set of integration tests
    As per other vCloud Tools, we want jenkins to run the unit tests, features and integration tests, and if all that passes, to publish (this commit doesn't do the publication, that is a subsequent commit). Here, we run the integration tests against one environment (the job in Jenkins will determine which one) as that should be sufficient. However, we have access to other environments, so we also require a jenkins_integration_tests script for just running the integration tests.

--- jenkins.sh
+++ jenkins.sh
@@ -2,3 +2,8 @@
 set -e
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 bundle exec rake 
+
+./scripts/generate_fog_conf_file.sh
+export FOG_RC=fog_integration_test.config
+bundle exec rake integration:all
+rm fog_integration_test.config
