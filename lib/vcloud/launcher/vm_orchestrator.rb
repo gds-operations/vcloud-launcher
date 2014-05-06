f6645048f812d5d9b1d2c8c543f88789b8dd9d99
Dan Carley <dan.carley@gmail.com>
2014-05-06 16:01:39 +0100

    Replace assignment in condition for VM
    Fixes lint warning. This isn't a whole lot more characters and is a little
    bit more readable.

--- lib/vcloud/launcher/vm_orchestrator.rb
+++ lib/vcloud/launcher/vm_orchestrator.rb
@@ -10,9 +10,9 @@ module Vcloud
         @vm.update_name(@vm.vapp_name)
         @vm.configure_network_interfaces vm_config[:network_connections]
         @vm.update_storage_profile(vm_config[:storage_profile]) if vm_config[:storage_profile]
-        if hardware_config = vm_config[:hardware_config]
-          @vm.update_cpu_count(hardware_config[:cpu])
-          @vm.update_memory_size_in_mb(hardware_config[:memory])
+        if vm_config[:hardware_config]
+          @vm.update_cpu_count(vm_config[:hardware_config][:cpu])
+          @vm.update_memory_size_in_mb(vm_config[:hardware_config][:memory])
         end
         @vm.add_extra_disks(vm_config[:extra_disks])
         @vm.update_metadata(vm_config[:metadata])
