229559e7f8bc59e64f34aa9496f824cb2ce0e830
Dan Carley <dan.carley@gmail.com>
2014-05-15 11:18:05 +0100

    Remove outer module block from unit tests
    So that our tests aren't extending and implicitly namespaced into the
    `Vcloud` module. This will make it more obvious where we're crossing the
    boundaries of different projects. It will also instantiate the class given
    (without any args) and make it available as `subject` which may allow us to
    DRY up some of our tests.

--- spec/vcloud/launcher/launch_spec.rb
+++ spec/vcloud/launcher/launch_spec.rb
@@ -1,76 +1,74 @@
 require 'spec_helper'
 
-module Vcloud
-  describe Launcher::Launch do
-    context "#run" do
-      before(:each) do
-        config_loader = double(:config_loader)
-        expect(Vcloud::Core::ConfigLoader).to receive(:new).and_return(config_loader)
-        @successful_app_1 = {
-            :name => "successful app 1",
-            :vdc_name => "Test Vdc",
-            :catalog => "default",
-            :catalog_item => "ubuntu-precise"
-        }
-        @fake_failing_app = {
-            :name => "fake failing app",
-            :vdc_name => "wrong vdc",
-            :catalog => "default",
-            :catalog_item => "ubuntu-precise"
-        }
-        @successful_app_2 = {
-            :name => "successful app 2",
-            :vdc_name => "Test Vdc",
-            :catalog => "default",
-            :catalog_item => "ubuntu-precise"
-        }
-        expect(config_loader).to receive(:load_config).
-          and_return({:vapps => [@successful_app_1, @fake_failing_app, @successful_app_2]})
-      end
-
-      it "should stop on failure by default" do
-        expect(Vcloud::Launcher::VappOrchestrator).to receive(:provision).with(@successful_app_1).and_return(double(:vapp, :power_on => true))
-        expect(Vcloud::Launcher::VappOrchestrator).to receive(:provision).with(@fake_failing_app).and_raise(RuntimeError.new('failed to find vdc'))
-        expect(Vcloud::Launcher::VappOrchestrator).not_to receive(:provision).with(@successful_app_2)
-
-        cli_options = {}
-        Vcloud::Launcher::Launch.new.run('input_config_yaml', cli_options)
-      end
-
-      it "should continue on error if cli option continue-on-error is set" do
-        expect(Vcloud::Launcher::VappOrchestrator).to receive(:provision).with(@successful_app_1).and_return(double(:vapp, :power_on => true))
-        expect(Vcloud::Launcher::VappOrchestrator).to receive(:provision).with(@fake_failing_app).and_raise(RuntimeError.new('failed to find vdc'))
-        expect(Vcloud::Launcher::VappOrchestrator).to receive(:provision).with(@successful_app_2).and_return(double(:vapp, :power_on => true))
-
-        cli_options = {"continue-on-error" => true}
-        Vcloud::Launcher::Launch.new.run('input_config_yaml', cli_options)
-      end
-
+describe Vcloud::Launcher::Launch do
+  context "#run" do
+    before(:each) do
+      config_loader = double(:config_loader)
+      expect(Vcloud::Core::ConfigLoader).to receive(:new).and_return(config_loader)
+      @successful_app_1 = {
+          :name => "successful app 1",
+          :vdc_name => "Test Vdc",
+          :catalog => "default",
+          :catalog_item => "ubuntu-precise"
+      }
+      @fake_failing_app = {
+          :name => "fake failing app",
+          :vdc_name => "wrong vdc",
+          :catalog => "default",
+          :catalog_item => "ubuntu-precise"
+      }
+      @successful_app_2 = {
+          :name => "successful app 2",
+          :vdc_name => "Test Vdc",
+          :catalog => "default",
+          :catalog_item => "ubuntu-precise"
+      }
+      expect(config_loader).to receive(:load_config).
+        and_return({:vapps => [@successful_app_1, @fake_failing_app, @successful_app_2]})
     end
 
-    context "#set_logging_level" do
+    it "should stop on failure by default" do
+      expect(Vcloud::Launcher::VappOrchestrator).to receive(:provision).with(@successful_app_1).and_return(double(:vapp, :power_on => true))
+      expect(Vcloud::Launcher::VappOrchestrator).to receive(:provision).with(@fake_failing_app).and_raise(RuntimeError.new('failed to find vdc'))
+      expect(Vcloud::Launcher::VappOrchestrator).not_to receive(:provision).with(@successful_app_2)
 
-      it "sets the logging level to DEBUG when :verbose is specified" do
-        expect(Vcloud::Core.logger).to receive(:level=).with(Logger::DEBUG)
-        Vcloud::Launcher::Launch.new.set_logging_level(:verbose => true)
-      end
+      cli_options = {}
+      Vcloud::Launcher::Launch.new.run('input_config_yaml', cli_options)
+    end
 
-      it "sets the logging level to ERROR when :quiet is specified" do
-        expect(Vcloud::Core.logger).to receive(:level=).with(Logger::ERROR)
-        Vcloud::Launcher::Launch.new.set_logging_level(:quiet => true)
-      end
+    it "should continue on error if cli option continue-on-error is set" do
+      expect(Vcloud::Launcher::VappOrchestrator).to receive(:provision).with(@successful_app_1).and_return(double(:vapp, :power_on => true))
+      expect(Vcloud::Launcher::VappOrchestrator).to receive(:provision).with(@fake_failing_app).and_raise(RuntimeError.new('failed to find vdc'))
+      expect(Vcloud::Launcher::VappOrchestrator).to receive(:provision).with(@successful_app_2).and_return(double(:vapp, :power_on => true))
 
-      it "sets the logging level to DEBUG when :quiet and :verbose are specified" do
-        expect(Vcloud::Core.logger).to receive(:level=).with(Logger::DEBUG)
-        Vcloud::Launcher::Launch.new.set_logging_level(:quiet => true, :verbose => true)
-      end
-
-      it "sets the logging level to INFO by default" do
-        expect(Vcloud::Core.logger).to receive(:level=).with(Logger::INFO)
-        Vcloud::Launcher::Launch.new.set_logging_level({})
-      end
-
+      cli_options = {"continue-on-error" => true}
+      Vcloud::Launcher::Launch.new.run('input_config_yaml', cli_options)
     end
 
   end
+
+  context "#set_logging_level" do
+
+    it "sets the logging level to DEBUG when :verbose is specified" do
+      expect(Vcloud::Core.logger).to receive(:level=).with(Logger::DEBUG)
+      Vcloud::Launcher::Launch.new.set_logging_level(:verbose => true)
+    end
+
+    it "sets the logging level to ERROR when :quiet is specified" do
+      expect(Vcloud::Core.logger).to receive(:level=).with(Logger::ERROR)
+      Vcloud::Launcher::Launch.new.set_logging_level(:quiet => true)
+    end
+
+    it "sets the logging level to DEBUG when :quiet and :verbose are specified" do
+      expect(Vcloud::Core.logger).to receive(:level=).with(Logger::DEBUG)
+      Vcloud::Launcher::Launch.new.set_logging_level(:quiet => true, :verbose => true)
+    end
+
+    it "sets the logging level to INFO by default" do
+      expect(Vcloud::Core.logger).to receive(:level=).with(Logger::INFO)
+      Vcloud::Launcher::Launch.new.set_logging_level({})
+    end
+
+  end
+
 end
--- spec/vcloud/launcher/vapp_orchestrator_spec.rb
+++ spec/vcloud/launcher/vapp_orchestrator_spec.rb
@@ -1,52 +1,48 @@
 require 'spec_helper'
 
-module Vcloud
-  module Launcher
-    describe VappOrchestrator do
+describe Vcloud::Launcher::VappOrchestrator do
 
-      context "provision a vapp" do
+  context "provision a vapp" do
 
-        before(:each) do
-          @config = {
-              :name => 'test-vapp-1',
-              :vdc_name => 'test-vdc-1',
-              :catalog => 'org-1-catalog',
-              :catalog_item => 'org-1-template',
-              :vm => {
-                  :network_connections => [{:name => 'org-vdc-1-net-1'}]
-              }
+    before(:each) do
+      @config = {
+          :name => 'test-vapp-1',
+          :vdc_name => 'test-vdc-1',
+          :catalog => 'org-1-catalog',
+          :catalog_item => 'org-1-template',
+          :vm => {
+              :network_connections => [{:name => 'org-vdc-1-net-1'}]
           }
-        end
-
-        it "should return a vapp if it already exists" do
-          existing_vapp = double(:vapp, :name => 'existing-vapp-1')
-
-          Vcloud::Core::Vapp.should_receive(:get_by_name_and_vdc_name).with('test-vapp-1', 'test-vdc-1').and_return(existing_vapp)
-          Vcloud::Core.logger.should_receive(:info).with('Found existing vApp test-vapp-1 in vDC \'test-vdc-1\'. Skipping.')
-          actual_vapp = Vcloud::Launcher::VappOrchestrator.provision @config
-          actual_vapp.should_not be_nil
-          actual_vapp.should == existing_vapp
-        end
-
-        it "should create a vapp if it does not exist" do
-          #this test highlights the problems in vapp
-          mock_fog_vm = double(:vm)
-          mock_vapp = double(:vapp, :fog_vms => [mock_fog_vm], :reload => self)
-          mock_vm_orchestrator = double(:vm_orchestrator, :customize => true)
-
-
-          Vcloud::Core::Vapp.should_receive(:get_by_name_and_vdc_name).with('test-vapp-1', 'test-vdc-1').and_return(nil)
-          Vcloud::Core::VappTemplate.should_receive(:get).with('org-1-catalog', 'org-1-template').and_return(double(:vapp_template, :id => 1))
-
-          Vcloud::Core::Vapp.should_receive(:instantiate).with('test-vapp-1', ['org-vdc-1-net-1'], 1, 'test-vdc-1')
-          .and_return(mock_vapp)
-          Vcloud::Launcher::VmOrchestrator.should_receive(:new).with(mock_fog_vm, mock_vapp).and_return(mock_vm_orchestrator)
-
-          new_vapp = Vcloud::Launcher::VappOrchestrator.provision @config
-          new_vapp.should == mock_vapp
-        end
-
-      end
+      }
     end
+
+    it "should return a vapp if it already exists" do
+      existing_vapp = double(:vapp, :name => 'existing-vapp-1')
+
+      Vcloud::Core::Vapp.should_receive(:get_by_name_and_vdc_name).with('test-vapp-1', 'test-vdc-1').and_return(existing_vapp)
+      Vcloud::Core.logger.should_receive(:info).with('Found existing vApp test-vapp-1 in vDC \'test-vdc-1\'. Skipping.')
+      actual_vapp = Vcloud::Launcher::VappOrchestrator.provision @config
+      actual_vapp.should_not be_nil
+      actual_vapp.should == existing_vapp
+    end
+
+    it "should create a vapp if it does not exist" do
+      #this test highlights the problems in vapp
+      mock_fog_vm = double(:vm)
+      mock_vapp = double(:vapp, :fog_vms => [mock_fog_vm], :reload => self)
+      mock_vm_orchestrator = double(:vm_orchestrator, :customize => true)
+
+
+      Vcloud::Core::Vapp.should_receive(:get_by_name_and_vdc_name).with('test-vapp-1', 'test-vdc-1').and_return(nil)
+      Vcloud::Core::VappTemplate.should_receive(:get).with('org-1-catalog', 'org-1-template').and_return(double(:vapp_template, :id => 1))
+
+      Vcloud::Core::Vapp.should_receive(:instantiate).with('test-vapp-1', ['org-vdc-1-net-1'], 1, 'test-vdc-1')
+      .and_return(mock_vapp)
+      Vcloud::Launcher::VmOrchestrator.should_receive(:new).with(mock_fog_vm, mock_vapp).and_return(mock_vm_orchestrator)
+
+      new_vapp = Vcloud::Launcher::VappOrchestrator.provision @config
+      new_vapp.should == mock_vapp
+    end
+
   end
 end
