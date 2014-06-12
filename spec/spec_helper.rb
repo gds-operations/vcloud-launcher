85232bde7ae4ef43a4db56275f214fc8de6a741e
Matt Bostock <matt@mattbostock.com>
2014-06-12 14:47:25 +0100

    Add back SimpleCov
    This partially reverts 5e0f412b17aa2faa5e2e71bc11dfa502a7f6ddb7 with the
    following differences:
    
    Configure the SimpleCov behaviour so that it doesn't report coverage
    when rake completes and acts only as a 'red light' if coverage drops
    significantly (by 20%) or drops below a percentage (80%).
    
    Note that to silence the 'Coverage report generated for' message that
    reports the coverage percentage when SimpleCov completes, we
    monkey-patch the SimpleCov::Formatter::HTMLFormatter class.
    
    Also, SimpleCov#result.format! is called by default when SimpleCov
    exits[1], so there's no need for us to explicitly call it.
    
    The SimpleCov::Formatter::HTMLFormatter#output_message only prints the
    'Coverage report generated for' message, so by monkey-patching it we
    don't lose the other warnings that we want to keep (mentioned above).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,27 @@
+# SimpleCov must run _first_ according to its README
+if ENV['COVERAGE']
+  require 'simplecov'
+
+  # monkey-patch to prevent SimpleCov from reporting coverage percentage
+  class SimpleCov::Formatter::HTMLFormatter
+    def output_message(_message)
+      nil
+    end
+  end
+
+  SimpleCov.profiles.define 'gem' do
+    add_filter '/spec/'
+    add_filter '/features/'
+    add_filter '/vendor/'
+
+    add_group 'Libraries', '/lib/'
+  end
+
+  SimpleCov.minimum_coverage(80)
+  SimpleCov.maximum_coverage_drop(20)
+  SimpleCov.start 'gem'
+end
+
 require 'erb_helper'
 require 'vcloud/launcher'
 
--- vcloud-launcher.gemspec
+++ vcloud-launcher.gemspec
@@ -26,5 +26,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rake'
   s.add_development_dependency 'rspec', '~> 2.14.1'
   s.add_development_dependency 'rubocop', '~> 0.23.0'
+  s.add_development_dependency 'simplecov', '~> 0.8.2'
   s.add_development_dependency 'vcloud-tools-tester', '0.0.3'
 end
