51d0936bf1cbad14a6d151c748fc19418b84f69f
Barrie Bremner <baz@barriebremner.com>
2014-06-29 20:43:53 +0100

    Ensure that preamble class is invoked during customization.

--- spec/vcloud/launcher/vm_orchestrator_spec.rb
+++ spec/vcloud/launcher/vm_orchestrator_spec.rb
@@ -54,7 +54,8 @@ describe Vcloud::Launcher::VmOrchestrator do
     expect(vm).to receive(:update_memory_size_in_mb).with(4096)
     expect(vm).to receive(:add_extra_disks).with(vm_config[:extra_disks])
     expect(vm).to receive(:update_metadata).with(vm_config[:metadata])
-    expect(vm).to receive(:configure_guest_customization_section).with('web-app1', vm_config[:bootstrap], vm_config[:extra_disks])
+
+    allow(vm).to receive(:configure_guest_customization_section)
 
     subject.customize(vm_config)
   end
@@ -74,10 +75,72 @@ describe Vcloud::Launcher::VmOrchestrator do
     expect(vm).to receive(:update_name).with('web-app1')
     expect(vm).to receive(:add_extra_disks).with(vm_config[:extra_disks])
     expect(vm).to receive(:configure_network_interfaces).with(vm_config[:network_connections])
-    expect(vm).to receive(:configure_guest_customization_section).with('web-app1', vm_config[:bootstrap], vm_config[:extra_disks])
+
+    allow(vm).to receive(:configure_guest_customization_section)
 
     subject.customize(vm_config)
+  end
 
+  context "when customizing a VM" do
+    let(:vm_config) do
+      {
+        hardware_config: {
+          memory: 4096,
+          cpu: 2
+        },
+        network_connections: [
+          { name: "network1", ip_address: "198.12.1.21" }
+        ],
+        bootstrap: {
+          script_path: '/tmp/boostrap.erb',
+          vars: { message: 'hello world' }
+        }
+      }
+    end
+
+    let(:vm) { double(:vm, id: @vm_id, vapp_name: 'web-app1', vapp: vapp, name: 'test-vm-1') }
+
+    before do
+      Vcloud::Core::Vm.stub(:new).and_return(vm)
+      allow(vm).to receive(:update_name)
+      allow(vm).to receive(:configure_network_interfaces)
+      allow(vm).to receive(:update_cpu_count)
+      allow(vm).to receive(:update_memory_size_in_mb)
+      allow(vm).to receive(:add_extra_disks)
+      allow(vm).to receive(:update_metadata)
+    end
+
+    context "Vcloud::Launcher::Preamble used to generate a preamble" do
+      it "instantiates Vcloud::Launcher::Preamble" do
+        preamble = double
+        allow(preamble).to receive(:generate)
+        allow(vm).to receive(:configure_guest_customization_section)
+
+        expect(Vcloud::Launcher::Preamble).to receive(:new).with(vm.vapp_name, vm_config).and_return(preamble)
+
+        subject.customize(vm_config)
+      end
+
+      it "uses Vcloud::Launcher::Preamble.generate to template a preamble" do
+        preamble = double
+        allow(Vcloud::Launcher::Preamble).to receive(:new).with(vm.vapp_name, vm_config).and_return(preamble)
+        allow(vm).to receive(:configure_guest_customization_section)
+
+        expect(preamble).to receive(:generate)
+
+        subject.customize(vm_config)
+      end
+
+      it "passes the generated preamble to configure_guest_customization_section" do
+        preamble = double
+        allow(Vcloud::Launcher::Preamble).to receive(:new).with(vm.vapp_name, vm_config).and_return(preamble)
+        allow(preamble).to receive(:generate).and_return('FAKE_PREAMBLE')
+
+        expect(vm).to receive(:configure_guest_customization_section).with('FAKE_PREAMBLE')
+
+        subject.customize(vm_config)
+      end
+    end
   end
 
   context "when generating a preamble" do
